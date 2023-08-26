6a5af3aefd0eceb3e914aa28576968947dd88508
struk49 <>
2023-08-26 11:29:09 +0000

    Added session user profile

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-
+        return redirect(url_for('profile', username=session["user"]))
     return render_template("register.html")
 
 
@@ -66,8 +66,11 @@ def signin():
             # ensure hashed password matches user input
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    'profile', username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -79,6 +82,15 @@ def signin():
             return redirect(url_for("login"))
 
     return render_template("signin.html")
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+
 
 
 if __name__ == "__main__":
