5f3a5d0831f868e2e6932547baf29ea53e0847ab
struk49 <>
2023-08-27 15:41:36 +0000

    Create add_book template

--- app.py
+++ app.py
@@ -104,6 +104,11 @@ def signout():
     return redirect(url_for('signin'))
 
 
+@app.route("/add_book")
+def add_book():
+    return render_template("add_book.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
