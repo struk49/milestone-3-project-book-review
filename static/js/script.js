be85831e6eadc44a6dd01d83b82a590c9058ec78
struk49 <>
2023-09-07 11:59:14 +0000

    corrected javascript and adjusted screen size function

--- static/js/script.js
+++ static/js/script.js
@@ -14,9 +14,7 @@ $(document).ready(function () {
         i18n: {
             done: "Select"
         }
-        $(document).ready(function(){
-            $('.carousel').carousel();
-          });
+       
     });
 
     validateMaterializeSelect();
